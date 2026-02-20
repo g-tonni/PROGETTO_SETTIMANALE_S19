d479735bfce81c144843f499428ac879c96e301e
g-tonni <tonni.giada.210802@gmail.com>
2026-02-20 15:23:44 +0100

    AGGIUNTE AUTORIZZAZIONI PRENOTAZIONE

--- src/main/java/giadatonni/PROGETTO_SETTIMANALE_S19/controllers/PrenotazioniController.java
+++ src/main/java/giadatonni/PROGETTO_SETTIMANALE_S19/controllers/PrenotazioniController.java
@@ -4,6 +4,8 @@ import giadatonni.PROGETTO_SETTIMANALE_S19.entities.Prenotazione;
 import giadatonni.PROGETTO_SETTIMANALE_S19.entities.Utente;
 import giadatonni.PROGETTO_SETTIMANALE_S19.payloads.PrenotazioneDTO;
 import giadatonni.PROGETTO_SETTIMANALE_S19.services.PrenotazioniService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +20,8 @@ public class PrenotazioniController {
     }
 
     @PostMapping
-    @ResponseStatus
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyAuthority('UTENTE', 'ORGANIZZATORE')")
     public Prenotazione postPrenotazione(@RequestBody PrenotazioneDTO body, @AuthenticationPrincipal Utente utenteAutenticato){
         return this.prenotazioniService.postPrenotazione(body, utenteAutenticato.getUtenteId());
     }
