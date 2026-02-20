8142892db7a19ca4937ad3049dc1b94e320d09cf
g-tonni <tonni.giada.210802@gmail.com>
2026-02-20 09:02:01 +0100

    Initial commit

---
+++ src/main/java/giadatonni/PROGETTO_SETTIMANALE_S19/ProgettoSettimanaleS19Application.java
@@ -0,0 +1,13 @@
+package giadatonni.PROGETTO_SETTIMANALE_S19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanaleS19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanaleS19Application.class, args);
+	}
+
+}
---
+++ src/test/java/giadatonni/PROGETTO_SETTIMANALE_S19/ProgettoSettimanaleS19ApplicationTests.java
@@ -0,0 +1,13 @@
+package giadatonni.PROGETTO_SETTIMANALE_S19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleS19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
